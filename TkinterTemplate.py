50a8daa842339bc96240b8c598af30b9fbfd3ca4
zh421 <dd0042187@gmail.com>
2023-04-23 10:45:47 +0800

    first commit

---
+++ TkinterTemplate.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import messagebox
+import menuBar
+import widgetsPage as wp
+import openWindow as ow
+from morePage import PageOne, PageTwo, PageThree, PageFour
+
+"""
+Useful Links:
+https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter Most useful in my opinion
+https://www.tutorialspoint.com/python/python_gui_programming.htm
+https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/index.html
+https://www.youtube.com/watch?v=HjNHATw6XgY&list=PLQVvvaa0QuDclKx-QpC9wntnURXVJqLyk
+"""
+
+# You can also use a pandas dataframe for pokemon_info.
+# you can convert the dataframe using df.to_numpy.tolist()
+
+
+class MyApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        main_frame = tk.Frame(self, height=600, width=1024)
+        main_frame.pack_propagate(0)
+        main_frame.pack(fill="both", expand="true")
+        main_frame.grid_rowconfigure(0, weight=1)
+        main_frame.grid_columnconfigure(0, weight=1)
+        # self.resizable(0, 0) prevents the app from being resized
+        # self.geometry("1024x600") fixes the applications size
+        self.frames = {}
+        pages = (wp.Some_Widgets, PageOne, PageTwo, PageThree, PageFour)
+        for F in pages:
+            frame = F(main_frame, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(wp.Some_Widgets)
+        menubar = menuBar.MenuBar(self)
+        tk.Tk.config(self, menu=menubar)
+
+    def show_frame(self, name):
+        frame = self.frames[name]
+        frame.tkraise()
+
+    def OpenNewWindow(self):
+        ow.OpenNewWindow()
+
+    def Quit_application(self):
+        self.destroy()
+
+
+
---
+++ menuBar.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import widgetsPage as wp
+import morePage
+
+class MenuBar(tk.Menu):
+    def __init__(self, parent):
+        tk.Menu.__init__(self, parent)
+
+        menu_file = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="File", menu=menu_file)
+        menu_file.add_command(label="All Widgets", command=lambda: parent.show_frame(wp.Some_Widgets))
+        menu_file.add_separator()
+        menu_file.add_command(label="Exit Application", command=lambda: parent.Quit_application())
+
+        menu_orders = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu2", menu=menu_orders)
+
+        menu_pricing = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu3", menu=menu_pricing)
+        menu_pricing.add_command(label="Page One", command=lambda: parent.show_frame(morePage.PageOne))
+
+        menu_operations = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu4", menu=menu_operations)
+        menu_operations.add_command(label="Page Two", command=lambda: parent.show_frame(morePage.PageTwo))
+        menu_positions = tk.Menu(menu_operations, tearoff=0)
+        menu_operations.add_cascade(label="Menu5", menu=menu_positions)
+        menu_positions.add_command(label="Page Three", command=lambda: parent.show_frame(morePage.PageThree))
+        menu_positions.add_command(label="Page Four", command=lambda: parent.show_frame(morePage.PageFour))
+
+        menu_help = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu6", menu=menu_help)
+        menu_help.add_command(label="Open New Window", command=lambda: parent.OpenNewWindow())
